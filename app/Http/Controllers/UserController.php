0589731304edf0a78a812547e1c3e549da996dbf
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-25 14:38:19 +0530

    feat : Register api and login api build

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('user:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'fullname'=>'required|string|between:3,15',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/',
+            'mobile'=>'required|digits:10'
+        ]);
+
+        $user = new User([
+            'fullname'=> $request->input('fullname'),
+            'email'=> $request->input('email'),
+            'password'=> bcrypt($request->input('password')),
+            'mobile'=> $request->input('mobile'),
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message'=>'Successfully Created user'
+        ],201);    
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid Credentials'], 401);
+            }
+        }catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'],500);
+        }
+        return response()->json(['token' => $token], 200);
+    }
+}
