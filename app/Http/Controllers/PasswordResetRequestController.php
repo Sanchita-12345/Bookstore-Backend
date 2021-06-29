f1fb3279acbaa04a3d1c7bca05c3221346c6bc4c
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-29 21:03:48 +0530

    build : commenting added for PasswordResetRequestController

--- app/Http/Controllers/PasswordResetRequestController.php
+++ app/Http/Controllers/PasswordResetRequestController.php
@@ -8,62 +8,37 @@ use App\Models\User;
 use App\Mail\SendMailreset;
 use Illuminate\Support\Facades\Mail;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use App\Models\PasswordReset;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Notifications\ResetPasswordNotification;
+
+/**
+ * @description class PasswordResetRequestController
+ * @class PasswordResetRequestController
+ * @extends Controller
+ * purpose     :sending the mail to the user who forgot password
+ * @since      : 28-06-2021
+ */
+
 class PasswordResetRequestController extends Controller
 {
-    public function sendEmail(Request $request)  // this is most important function to send mail and inside of that there are another function
+    public function forgotPassword(Request $request)
     {
-        if (!$this->validateEmail($request->email)) {  // this is validate to fail send mail or true
-            return $this->failedResponse();
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json(['error' => 'Email doesn\'t found on our database'],Response::HTTP_NOT_FOUND);
         }
-        $this->send($request->email);  //this is a function to send mail 
-        return $this->successResponse();
-    }
-    //this is a function to send mail 
-    public function send($email)  
-    {
-        $token = $this->createToken($email);
-        Mail::to($email)->send(new SendMailreset($token, $email)); 
-         // token is important in send mail 
-    }
-    public function createToken($email) 
-     // this is a function to get your request email that there are or not to send mail
-    {
-        $oldToken = DB::table('password_resets')->where('email', $email)->first();
-
-        if ($oldToken) {
-            return $oldToken->token;
+        $passwordReset = PasswordReset::updateOrCreate(
+            ['email' => $user->email],
+            [
+                'email' => $user->email,
+                'token' => JWTAuth::fromUser($user)
+            ]
+        );
+        if ($user && $passwordReset) {
+            $user->notify(new ResetPasswordNotification($passwordReset->token));
         }
-
-        $token = Str::random(40);  
-        $this->saveToken($token, $email);
-        return $token;
-    }
-    public function saveToken($token, $email)  // this function save new password
-    {
-        DB::table('password_resets')->insert([
-            'email' => $email,
-            'token' => $token,
-            'created_at' => Carbon::now() //created  time string
-        ]);
-    }
-    public function validateEmail($email)  //this is a function to get your email from database
-    {
-        return !!User::where('email', $email)->first();
-    }
-
-    public function failedResponse()
-    {
-        return response()->json([
-            'error' => 'Email does\'t found on our database'
-        ], Response::HTTP_NOT_FOUND);
-    }
-
-    public function successResponse()
-    {
-        return response()->json([
-            'data' => 'Reset link is send successfully, please check your inbox.'
-        ], Response::HTTP_OK);
+        return response()->json(['data' => 'Reset link is send successfully, please check your inbox.'], Response::HTTP_OK);
     }
 }
+    
