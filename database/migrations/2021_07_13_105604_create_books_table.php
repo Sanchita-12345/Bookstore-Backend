62fbfdaab2a787805bcb078962fd4358b1484bd2
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-07-13 22:10:28 +0530

    modify : cart default value attribute added

---
+++ database/migrations/2021_07_13_105604_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('author');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->string('file')->nullable();
+            $table->longText('description');
+            $table->unsignedBigInteger('user_id');
+            $table->enum('cart', [0, 1])->default(0);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
